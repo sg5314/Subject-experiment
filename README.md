a36baeabb140daed6f4e5cdbda580d3c0382cbdb
sg5314 <88835817+sg5314@users.noreply.github.com>
2023-02-21 12:32:55 +0900

    add_gin_subject_experiment

---
+++ README.md
@@ -0,0 +1,14 @@
+# Subject-experiment
+
+```
+docker-compose build
+docker compose up -d 
+```
+
+```
+docker-compose exec server sh
+```
+
+```
+docker-compose down -v
+```
---
+++ server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	// "strconv"
+	"fmt"
+
+	"github.com/gin-contrib/sessions"
+    "github.com/gin-contrib/sessions/cookie"
+)
+
+
+
+
+
+func main() {
+
+	router := gin.Default()
+	router.LoadHTMLGlob("./templates/*.html")
+	router.Static("/img", "./img/")
+
+	// セッションの設定
+	store := cookie.NewStore([]byte("secret"))
+    router.Use(sessions.Sessions("mysession", store))
+
+	// router.GET("/research/:route/:page/", func(c *gin.Context) {
+	// 	var page_temp int
+	// 	route := c.Param("route")
+	// 	page_temp, _ = strconv.Atoi(c.Param("page"))
+	// 	page := fmt.Sprintf("%04d.jpg", page_temp)
+	// 	fmt.Println(route, page)
+		
+	// 	c.HTML(http.StatusOK, "research.html", gin.H{
+	// 		"route": "living_room",
+	// 		"page": "0000.jpg",
+	// 	})
+	// })
+
+	var page int = 0
+
+	router.GET("/research/", func(c *gin.Context) {
+
+		page = 0
+		
+		c.HTML(http.StatusOK, "research.html", gin.H{
+			"route": "living_room",
+			"page": "0000.jpg",
+		})
+	})
+
+	router.POST("/post/", func(c *gin.Context) {
+		s := c.PostForm("str")
+		fmt.Println(s)
+		page+=1
+		c.HTML(http.StatusOK, "research.html", gin.H{
+			"route": "living_room",
+			"page": fmt.Sprintf("%04d.jpg", page),
+		})
+	})
+
+	router.Run(":3000") // 0.0.0.0:8080 でサーバーを立てます。
+
+}
